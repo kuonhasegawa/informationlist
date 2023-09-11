f0e80b7319ff80b485646630de354887f46e98c9
kuonhasegawa <kuon.hasegawa@gmail.com>
2023-09-11 13:59:37 +0900

    1回目のコミット新規

---
+++ informationlist/src/test/java/jp/gihyo/snowman/informationlist/InformationlistApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gihyo.snowman.informationlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
